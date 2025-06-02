5f30b4cccb1957c16f603e6fce9e6463a94636ab
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2025-06-02 22:06:55 +0800

    Add .gitignore, update dependencies to use axios, and bump version to 1.0.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var request = require('request');
+var axios = require('axios');
 var Promise = require('bluebird');
 var plantuml = require('./plantuml');
 var assign = require('object-assign');
@@ -14,12 +14,17 @@ hexo.extend.tag.register('plantuml', function(args, content){
     return new Promise(function (resolve, reject) {
         var plantumlSvgUrl = plantuml.compress(content);
         if(hexo.config.tag_plantuml.type==='static') {
-            request(plantumlSvgUrl, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    resolve('<img src="data:image/svg+xml;utf8,'+encodeURIComponent(body)+'">');
-
-                }
-            });
+            axios.get(plantumlSvgUrl)
+                .then(function(response) {
+                    if (response.status === 200) {
+                        resolve('<img src="data:image/svg+xml;utf8,'+encodeURIComponent(response.data)+'">');
+                    } else {
+                        reject(new Error('Failed to fetch PlantUML image: ' + response.status));
+                    }
+                })
+                .catch(function(error) {
+                    reject(error);
+                });
         } else {
             resolve('<img src="'+plantumlSvgUrl+'">');
         }
